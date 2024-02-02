97b47897a08eaf0a020402d1fb8c01a3e6ed6881
Aoba <45892714+AobaKuma@users.noreply.github.com>
2024-02-02 16:09:07 +0800

    機制填寫，翻譯修復與補完。

--- Source/Fortification/Harmony/Harmony_Verb_Shoot.cs
+++ Source/Fortification/Harmony/Harmony_Verb_Shoot.cs
@@ -23,21 +23,14 @@ namespace Fortification
                     }
                     castPawn.records?.Increment(RecordDefOf.ShotsFired);
                 }
-                if (__instance.EquipmentSource.GetComp<CompCastPushHeat>() is CompCastPushHeat compCastPushHeat)            //射擊加溫
+                if (__instance.EquipmentSource?.GetComp<CompCastPushHeat>() is CompCastPushHeat compCastPushHeat)            //射擊加溫
                 {
                     if (compCastPushHeat.EnergyPerCast != 0)
                     {
                         GenTemperature.PushHeat(__instance.Caster.Position, __instance.Caster.Map, compCastPushHeat.EnergyPerCast);
                     }
                 }
-                //if (__instance.EquipmentSource.GetComps<CompCastEffecter>().EnumerableCount() != 0)
-                //{
-                //    foreach (var comp in __instance.EquipmentSource.GetComps<CompCastEffecter>())
-                //    {
-                //        comp.DoBursting(Vector3Utility.AngleToFlat(__instance.Caster.DrawPos, __instance.CurrentTarget.Cell.ToVector3Shifted()), __instance.Caster);
-                //    }
-                //}
-                if (__instance.EquipmentSource.GetComps<CompCastFlecker>().EnumerableCount() != 0)            //發射特效
+                if (__instance.EquipmentSource?.GetComps<CompCastFlecker>().EnumerableCount() != 0)            //發射特效
                 {
                     foreach (var comp in __instance.EquipmentSource.GetComps<CompCastFlecker>())
                     {

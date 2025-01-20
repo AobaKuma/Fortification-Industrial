3b6f56386695293dd9146afa723fd4bdd6d361e8
Aoba <sakilomo@gmail.com>
2025-01-20 17:16:23 +0800

    x

--- Source/Fortification/Comps/GameComponent_NuclearWarfare.cs
+++ Source/Fortification/Comps/GameComponent_NuclearWarfare.cs
@@ -10,22 +10,18 @@ namespace Fortification
         public GameComponent_NuclearWarfare(Game game)
         {
         }
-
         public int nuclearWeaponInStock = 0;
         public int nuclearWeaponDetonated = 0;
-
         private bool isNuclearWar = false;
         public  bool NuclearWarReached => !isNuclearWar && nuclearWeaponDetonated > 3;
 
         readonly String letter_init = "核鎮攝";
-
         readonly String letter_first = "核威脅";
         readonly String letter_second = "核陰雲";
         readonly String letter_third = "核競賽";
         readonly String letter_forth = "核制裁";
 
-        readonly String init = "很顯然你已經獲取了一枚核武器，但在這顆星球上的其他勢力顯然尚未知曉這一點。你可以在任何地點引爆核彈來向世界展示這份力量。";
-        
+        readonly String init = "很顯然你已經獲取了一枚核武器，但在這顆星球上的其他勢力顯然尚未知曉這一點。你可以在任何地點引爆核彈來向世界展示這份力量。";     
         readonly String first = "第一枚核彈成功的引爆並完成了它的設計目的，很快邊緣世界上的陣營都會知曉[陣營名稱]所掌握的力量，並重新審視交惡帶來的風險。\n\n你應當開始建設核武庫，每一顆庫存中的核彈都能在戰爭的談判桌壓上一枚籌碼。妥善保存這份力量，你便能夠在這顆星球上維持著由核武所帶來的和平。";
         readonly String second = "第二枚核彈摧毀的不只是目標，也同時正在破壞著星球上脆弱的信任。切記，核彈只有在躺在發射井中才能發揮它維繫和平的作用。\n\n或許一兩枚核彈還在他們接受的範圍，但我們無從知道其他陣營的底線。";
         readonly String third = "第三枚核彈的引爆很顯然刺激了星球上本地陣營的神經。\n\n一些勢力重新啟用了他們封存的核武庫與生產設施，引發了連鎖反應式的核武軍備競賽，很顯然這會使得那些不被監管的核彈流落到黑市與海盜的手中。";
